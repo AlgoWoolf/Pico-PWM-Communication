f67c8a87601b0c4090fb0044341bf61e3ce04001
Algo Woolf <114178509+AlgoWoolf@users.noreply.github.com>
2023-11-07 13:10:47 -0500

    commented

--- pico1.py
+++ pico1.py
@@ -2,21 +2,26 @@ import machine
 import utime
 import uos
 
-pwm_pin = machine.Pin(15)
-pwm = machine.PWM(pwm_pin)
-pwm.freq(50)
+# Configure the PWM pin and set its frequency
+pwm_pin = machine.Pin(15) # Define pin 15 as the PWM output pin.
+pwm = machine.PWM(pwm_pin) # Create a PWM object on pin 15.
+pwm.freq(50) # Set the PWM frequency to 50Hz.
 
-uart = machine.UART(1, baudrate=9600)
+# Configure UART
+uart = machine.UART(1, baudrate=9600) # Initialize UART 1 with a baud rate of 9600.
 
+# Function to set the PWM duty cycle and send the data via UART
 def set_pwm(duty_cycle, frequency=50):
     pwm.freq(frequency)
     pwm.duty_u16(duty_cycle)
-    send_pwm_data(duty_cycle, frequency)
+    send_pwm_data(duty_cycle, frequency) # Send the PWM data through UART.
 
+# Function to send PWM data through UART
 def send_pwm_data(value, frequency):
     message = 'START:{}:{}END'.format(value, frequency)
     uart.write(message)
 
+# Function to receive feedback from UART
 def receive_feedback():
     data = uart.read()
     if data:
--- pico2.py
+++ pico2.py
@@ -3,17 +3,19 @@ import utime
 
 uart = machine.UART(1, baudrate=9600)
 
+# Function to read PWM data from UART
 def read_pwm_data():
     data = uart.read()
     if data:
-        start_index = data.find(b'START')
-        end_index = data.find(b'END')
+        start_index = data.find(b'START') # Find the start of the data packet.
+        end_index = data.find(b'END') # Find the end of the data packet.
         if start_index != -1 and end_index != -1:
-            pwm_data = data[start_index+5:end_index].decode()
-            duty_cycle, frequency = pwm_data.split(":")
+            pwm_data = data[start_index+5:end_index].decode() # Decode the data between START and END.
+            duty_cycle, frequency = pwm_data.split(":") # Split the data to get duty cycle and frequency.
             return int(duty_cycle), int(frequency)
     return None, None
 
+# Function to send feedback through UART
 def send_feedback(duty_cycle, frequency):
     feedback = "Received PWM with duty cycle {} at {}Hz".format(duty_cycle, frequency)
     message = 'START{}END'.format(feedback)
